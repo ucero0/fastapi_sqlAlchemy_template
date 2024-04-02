077518bc150f0c6b51ebfc6c90e82d88346874e0
ucero0 <ucerozamar@gmail.com>
2024-04-02 03:36:49 +0200

    Update user authentication error handling

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -12,11 +12,11 @@ authRouter = APIRouter( tags=["auth"])
 @authRouter.post("/token", response_model=Token)
 async def login(user_credentials: Annotated[OAuth2PasswordRequestForm, Depends()], db: Annotated[Session,Depends(get_db)]):
     
-    userDb = userCRUD.read_user_by_username(db, user_credentials.username)
+    userDb = userCRUD.read_user_by_email(db, user_credentials.username)
     if not userDb:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
+        raise HTTPException(status_code=401, detail="Incorrect username or password")
     if not auth.verify_password(user_credentials.password, userDb.password):
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
+        raise HTTPException(status_code=401, detail="Incorrect username or password")
     tokenDAta = TokenData.model_validate(userDb)
     token = auth.create_access_token(tokenDAta.model_dump())
     return Token(access_token=token, token_type="bearer")
