8606469e471525bd969b22225857a6632d4344be
ucero0 <ucerozamar@gmail.com>
2024-04-02 03:37:09 +0200

    Update user credentials and add login and authorization tests

--- test/test_user.py
+++ test/test_user.py
@@ -3,18 +3,30 @@ from .conftest import client,session
 import pytest
 from ..app.routes.user import userRoute
 
-user_credentials = {"email": "ucero@gmail.com", "password": "password"}
+user_credentials = {"email": "test@gmail.com", "password": "password"}
+aouthSchema = {"username": user_credentials["email"], "password": user_credentials["password"]}
+
 @pytest.fixture
-def test_user(client):
+def createUser(client):
     response = client.post(userRoute.prefix,json=user_credentials,)
     print(response.json())
     assert response.status_code == 201
     newUser = response.json()
-
     return newUser
+@pytest.fixture
+def getToken(client):
+    response = client.post("/token",data=aouthSchema,)
+    return response.json()
 
 def test_connection_db(session):
     assert session is not None
+def test_login(client,createUser):
+    response = client.post("/token",data=aouthSchema,)
+    assert response.status_code == 200
+
+def test_login_invalid_user(client):
+    response = client.post("/token",data=aouthSchema,)
+    assert response.status_code == 401
 
 def test_create_user(client):
     response = client.post(userRoute.prefix,json=user_credentials,)
@@ -25,15 +37,24 @@ def test_create_user(client):
     response = client.post(userRoute.prefix,json=user_credentials,)
     assert response.status_code == 400 and response.json() == {"detail": "Email already registered"}
 
-
 def test_create_user_invalid_email(client):
     response = client.post(userRoute.prefix,
                            json={"email": "3453", "password": "password"}, )
     assert response.status_code == 422
 
-def test_get_users(client, test_user):
+def test_get_users(client, createUser):
     response = client.get(userRoute.prefix)
     assert response.status_code == 200
     data = response.json()
     assert len(data) > 0
 
+def test_get_myUser(client,createUser,getToken):
+    response = client.get(f"{userRoute.prefix}/me", headers={"Authorization": f"Bearer {getToken['access_token']}"})
+    assert response.status_code == 200
+    data = response.json()
+    assert data["email"] == user_credentials["email"]
+
+def test_delete_OwnUser(client,createUser,getToken):
+    print(createUser)
+    response = client.delete(f"{userRoute.prefix}/{createUser['email']}",headers={"Authorization": f"Bearer {getToken['access_token']}"}) 
+    assert response.status_code == 200
